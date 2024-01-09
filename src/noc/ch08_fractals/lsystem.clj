0500b2cc19929622c07d5e7f050701789879a554
Oleksandr Tykhonruk <a.tihonruk@gmail.com>
2024-01-09 16:14:50 +0200

    L-system animation + more examples.

--- src/noc/ch08_fractals/lsystem.clj
+++ src/noc/ch08_fractals/lsystem.clj
@@ -1,46 +1,108 @@
 (ns noc.ch08-fractals.sketch
   (:require [clojure.string :refer [join]])
-  (:require [quil.core :refer :all]))
+  (:require [quil.core :refer :all])
+  (:require [quil.middleware :refer [fun-mode]]))
 
 
-(def size 600)
+(def size 800)
 
 
-(defn lsystem
-  [axiom rules]
-  (iterate (fn [a] (join (map #(rules % %) a)))
-           axiom))
+(def tree
+  {:axiom "F"
+   :rules {\F "FF+[+F-F-F]-[-F+F+F]"}
+   :angle (radians 22.5)
+   :len   8})
+
+
+(def plant
+  {:axiom "X"
+   :rules {\F "FF"
+           \X "F-[[X]+X]+F[+FX]-X"}
+   :angle (radians 22.5)
+   :len   8})
+
+
+(def grid
+  {:axiom "F+F+F+F"
+   :rules {\F "FF+F-F+F+FF"}
+   :angle (radians 90)
+   :len 8})
+
+
+(def hilbert
+  {:axiom "X"
+   :rules {\X "-YF+XFX+FY-"
+           \Y "+XF-YFY-FX+"}
+   :angle (radians 90)
+   :len 20})
+;;   (translate 0 (height))
+
+
+(def dragon
+  {:axiom "FX"
+   :rules {\X "X+YF+"
+           \Y "-FX-Y"}
+   :angle (radians 90)
+   :len 5
+   })
+
+
+(def hex-gosper
+  {:axiom "XF"
+   :rules {\X "X+YF++YF-FX--FXFX-YF+"
+           \Y "-FX+YFYF++YF+FX--FX-Y"}
+   :angle (radians 60)
+   :len 6})
+
+
+(defn step [rules iter]
+  (join (map #(rules % %) iter)))
+
+
+(defn lsystem [{:keys [rules axiom] :as sys}]
+  "Lazy sequence of L-system generations. Current generation stored under the `:gen` key"
+  (iterate
+   #(update % :gen (partial step rules))
+   (assoc sys :gen axiom))) ;; initial generation
 
 
 (defn turtle
   "Draws bracketed string-based L-System"
-  [gen len theta]
+  [{:keys [gen len angle]}]
   (doseq [c gen]
     (case c
       \F (do (line 0 0 len 0) (translate len 0))
       \f (translate 0 len)
-      \+ (rotate theta)
-      \- (rotate (- theta))
+      \+ (rotate angle)
+      \- (rotate (- angle))
       \[ (push-matrix)
-      \] (pop-matrix))))
-
-
-(defn draw []
-  (background 255)
-  (translate (/ (width) 2) (height))
-  (rotate (- HALF-PI))
-  (stroke 0)
-  (let [axiom "F"
-        rules {\F "FF+[+F-F-F]-[-F+F+F]"}
-        sys (lsystem axiom rules)
-        gen (nth sys 4)
-        theta (radians 22.5)
-        len 10]
-    (turtle gen len theta)))
+      \] (pop-matrix)
+      nil)))
 
 
 (defn setup []
-  (no-loop))
+  (frame-rate 1)
+  (color-mode :hsb 360 100 100)
+  (background 45 5 100)
+  (lsystem tree))
+
+
+(defn update-state [[_ & rest]]
+  rest)
+
+
+(defn draw [[it & rest]]
+  (background 45 5 100)
+  ;; grounded
+  (translate (/ (width) 3) (height))
+  (rotate (- HALF-PI))
+  
+  ;; centered
+  ;; (translate (/ (width) 2) (/ (height) 2))
+
+  (turtle it)
+  (when (> (frame-count) 5)
+    (no-loop)))
 
 
 (defsketch lsystem-sketch
@@ -49,4 +111,6 @@
   :settings #(pixel-density (display-density))
   :setup setup
   :draw draw
-  :features [:keep-on-top])
+  :update update-state
+  :features [:keep-on-top]
+  :middleware [fun-mode])
